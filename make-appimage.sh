56f25db95aea8faad06e42b95ff231225528e5c9
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-22 17:26:49 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,6 +7,7 @@ APPNAME=wget
 VERSION=$(pacman -Q wget | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
+export APPNAME
 
 SHARUN_LINK=${SHARUN_LINK:-https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio}
 wget -qO /tmp/sharun-aio "$SHARUN_LINK"
