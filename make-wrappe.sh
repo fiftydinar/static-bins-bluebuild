4ed285b0af67303d6790e82499527a7568f794e7
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-23 09:59:06 +0100

    Simplify making bins now that `quick-sharun` has this option

--- make-wrappe.sh
+++ make-wrappe.sh
@@ -3,9 +3,6 @@
 set -eu
 
 ARCH=$(uname -m)
-SHARUN_LINK=${SHARUN_LINK:-https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio}
-wget -qO /tmp/sharun-aio "$SHARUN_LINK"
-chmod +x /tmp/sharun-aio
 
 APPNAME="
 	wget
@@ -16,8 +13,8 @@ APPNAME="
 
 for appname in $APPNAME; do
   VERSION=$(pacman -Q $appname | awk '{print $2; exit}')
-  /tmp/sharun-aio lib4bin --with-wrappe --dst-dir ./dist /usr/bin/$appname
-  mv -v ./dist/$appname ./dist/$appname-$VERSION-$ARCH-wrappe
+  quick-sharun --make-static-bin --dst-dir ./dist /usr/bin/"$appname"
+  mv -v ./dist/"$appname" ./dist/"$appname-$VERSION-$ARCH-wrappe"
 done
 
 APPNAME="
@@ -26,8 +23,8 @@ APPNAME="
 
 for appname in $APPNAME; do
   VERSION=$(pacman -Q findutils | awk '{print $2; exit}')
-  /tmp/sharun-aio lib4bin --with-wrappe --dst-dir ./dist /usr/bin/$appname
-  mv -v ./dist/$appname ./dist/$appname-$VERSION-$ARCH-wrappe
+  quick-sharun --make-static-bin --dst-dir ./dist /usr/bin/"$appname"
+  mv -v ./dist/"$appname" ./dist/"$appname-$VERSION-$ARCH-wrappe"
 done
 
 ###################################### coreutils ######################################
@@ -46,6 +43,6 @@ APPNAME="
 
 VERSION=$(pacman -Q coreutils | awk '{print $2; exit}')
 for appname in $APPNAME; do
-  /tmp/sharun-aio lib4bin --with-wrappe --dst-dir ./dist /usr/bin/"$appname"
-  mv -v ./dist/"$appname" ./dist/"$appname"-coreutils-"$VERSION"-$ARCH-wrappe
+  quick-sharun --make-static-bin --dst-dir ./dist /usr/bin/"$appname"
+  mv -v ./dist/"$appname" ./dist/"$appname-coreutils-$VERSION-$ARCH-wrappe"
 done
